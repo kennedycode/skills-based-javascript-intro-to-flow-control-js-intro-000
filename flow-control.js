cb8b4534cec2ae32bf263b042f11a226c9612319
Andrew Kennedy <akennedycode@gmail.com>
2020-07-16 19:51:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,44 +1,47 @@
 function basicTeenager(age){
-  If (age >= 13 && age <= 19){
-    return"You are a teenager!";
+  if (age >= 13 && age <=19){
+    return "You are a teenager!";
   }
 }
 
-function teenager(age) {
-  If (age >= 13 && age <=19){
+function teenager(age){
+  if (age >= 13 && age <=19){
     return "You are a teenager!";
-  } else {
+  }
+  else {
     return "You are not a teenager";
   }
 }
 
-function ageChecker(age) {
+function ageChecker(age){
   if (age >= 13 && age <=19){
     return "You are a teenager!";
   }
-  else if (age <13){
-  return "You are a kid";
-}else
-{
-  return "You are a grownup";
-}
-}
-
-function ternaryTeenager(age) {
-  return age >= 13 && age <=19? "You are a teenager" : "You are not a teenager"
+  else if (age < 13){
+    return "You are a kid";
+  }
+  else{
+    return "You are a grownup";
+  }
 }
 
-function switchAge(age) {
-  switch(age) {
+function ternaryTeenager(age){
+  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
+}
+
+function switchAge(age){
+  switch (age) {
   case 13:
   case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-  case 19:
-    return "You are a teenager"
+   case 15:
+   case 16:
+   case 17:
+   case 18:
+   case 19:
+    return "You are a teenager";
+    break;
   default:
-    return "You have an age";
-}
+    return "You have an age"
+  }
+
 }
